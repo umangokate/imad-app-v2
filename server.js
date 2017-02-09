d125516edb1a586268fee03e890419fe8424228c
umangokate <umangokate@gmail.com>
2017-02-09 10:40:12 -0700

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -12,6 +12,7 @@ app.get('/', function (req, res) {
 app.get('/article-one',function(req,res){
    res.send('Article one will be served here shortly') ;
 });
+
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
