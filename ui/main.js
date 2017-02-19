d3e35ac5c2b8534f68f93b514e35abebc4ca14d5
umangokate <umangokate@gmail.com>
2017-02-19 11:03:28 -0700

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,3 +1,9 @@
 console.log('Loaded!');
 var text = document.getElementById('main-text');
 text.innerHTML = 'Yala Yolo';
+
+var img = document.getElementById('madi');
+
+img.onclick =  function() {
+    img.style.marginLeft = '100px';
+};
