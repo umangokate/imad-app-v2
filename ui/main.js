0fd3fed8af6e485608a6267533af1ca3f77aaf2d
umangokate <umangokate@gmail.com>
2017-02-19 12:42:09 -0700

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,8 @@
-console.log('Hi');
+var button = document.getElementById('counter');
+var counter = document.getElementById('count');
+var cnt = 0;
+button.onclick = function()
+{
+    cnt = cnt+1;
+    counter.innerHTML = cnt.toString();
+};
