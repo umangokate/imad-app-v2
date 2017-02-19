48c08e63f5286dc70401aaec56732654f861b82c
umangokate <umangokate@gmail.com>
2017-02-19 12:24:23 -0700

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,14 +1,1 @@
-var counter = 0;
-var span = document.getElementById('');
-var request = new XMLHttpRequest();
-
-request.onreadystatechange = function()
-{
-    if (request.readyState === XMLHttpRequest.DONE)
-    {
-        if(request.status === 200)
-        {
-            var counter = counter+1;
-        }
-    }
-};
+console.log('Hi');
