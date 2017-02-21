7068db82b3478d7b2b172e302df03288a9b4c7f4
umangokate <umangokate@gmail.com>
2017-02-21 04:29:50 -0700

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -90,9 +90,9 @@ app.get('/counter',function(req,res){
 });
 
 var names = [];
-app.get('/submit-name/:name', function(req,res){
+app.get('/submit-name', function(req,res){
         
-        var name = req.params.name;
+        var name = req.query.name;
         names.push(name);
         res.send(JSON.stringify(names));
 });
