e63633b7fa3208da105cad6eb729375c75030473
umangokate <umangokate@gmail.com>
2017-02-21 01:21:06 -0700

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,8 +1,23 @@
 var button = document.getElementById('counter');
 var counter = document.getElementById('count');
-var cnt = 0;
-button.onclick = function()
-{
-    cnt = cnt+1;
-    counter.innerHTML = cnt.toString();
+
+
+button.onclick =  function(){
+    
+    var request = new XMLHttpRequest();
+    
+    request.readyonstatechange = function()
+    {
+      if(request.readyState == XMLHttpRequest.DONE)
+      {
+          if(request.status == 200)
+          {
+              var cnt = request.responseText;
+              counter.innerHTML = cnt.toString();
+              
+          }
+      }
+    };
 };
+request.open('GET', 'http://umangokate.imad.hasura-app.io/counter', true);
+request.send(null);
