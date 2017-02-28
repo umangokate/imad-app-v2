2c0adde3d7aab545337abe9e6a2a3d1dc3a5a5cc
umangokate <umangokate@gmail.com>
2017-02-28 09:10:18 -0700

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
 
+var Pool = require('pg').Pool;
+
 var app = express();
 app.use(morgan('combined'));
 
@@ -78,6 +80,24 @@ function createTemplate(data)
 return htmlTemp;
     
 }
+
+
+var pool = new Pool(config);
+
+app.get('/test-db',function(req,res){
+   
+   pool.query('SELECT * FROM test-db',function(err,result){
+      if(err)
+      {
+          res.status(500).send(err.toString());
+      }else
+      {
+          res.send(JSON.stringfy(result));
+      }
+       
+   });
+    
+});
 
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
